2f471919f529b36995e169e1d77641a33a3e8f4d
XYS612R <1149376304@qq.com>
2024-06-28 22:47:48 +0800

    2024-06-28

--- 71.cxx
+++ 71.cxx
@@ -22,13 +22,46 @@ public:
         cout << s << endl;
     }
 
-    string simplifyPath(string path) {
 
+    string simplifyPath(string path) {
+        auto split = [](const string& s,char delim){
+            vector<string> ans;
+            string cur;
+            for(auto c:s){
+                if(c==delim){
+                    ans.push_back(cur);
+                    cur.clear();
+                }else{
+                    cur+=c;
+                }
+            }
+            ans.push_back(cur);
+            return ans;
+        };
+        vector<string> seps = split(path,'/');
+        vector<string> stk;//只是借用了栈的概念
+        for(auto name:seps){
+            if(name == ".."){
+                if(!stk.empty())
+                    stk.pop_back();
+            }else if(!name.empty() && name != "."){
+                stk.push_back(name);
+            }
+        }
+        string ret;
+        if(stk.empty())
+            ret = "/";
+        for(auto c:stk){
+            ret += "/" + c;
+        }
+        return ret;
     }
 };
 
 int main()
 {
     Solution so;
+    string path = "/../";
+    cout<<so.simplifyPath(path)<<endl;
     return 0;
 }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # LeetCode Note
 
-# 228 
+# TS
+
+## T 228 
 双指针注意下表边界，用 i-1
 
 ```c++
@@ -29,3 +31,44 @@ public:
     }
 };
 ```
+
+## T 71
+
+简化路径
+
+```c++
+string simplifyPath(string path) {
+    auto split = [](const string& s,char delim){
+        vector<string> ans;
+        string cur;
+        for(auto c:s){
+            if(c==delim){
+                ans.push_back(cur);
+                cur.clear();
+            }else{
+                cur+=c;
+            }
+        }
+        ans.push_back(cur);
+        return ans;
+    };
+    vector<string> seps = split(path,'/');
+    vector<string> stk;//只是借用了栈的概念
+    for(auto name:seps){
+        if(name == ".."){
+            if(!stk.empty())
+                stk.pop_back();
+        }else if(!name.empty() && name != "."){
+            stk.push_back(name);
+        }
+    }
+    string ret;
+    if(stk.empty())
+        ret = "/";
+    for(auto c:stk){
+        ret += "/" + c;
+    }
+    return ret;
+}
+```
+
